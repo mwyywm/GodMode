b34c311eebcf7bd9bb005068ef3e6663c0faa5b3
mwyywm <31932525+mwyywm@users.noreply.github.com>
2025-04-08 01:12:40 +0200

    fix claude submit

--- src/providers/claude.js
+++ src/providers/claude.js
@@ -5,10 +5,9 @@ class Claude extends Provider {
 	static fullName = 'Anthropic Claude';
 	static shortName = 'Claude.ai';
 
-	static url = 'https://claude.ai/chats/';
+	static url = 'https://claude.ai/new';
 
 	static handleInput(input) {
-		const fullName = this.fullName;
 		this.getWebview().executeJavaScript(`{
     var inputElement = document.querySelector('div.ProseMirror')
 		if (inputElement) {
@@ -19,11 +18,8 @@ class Claude extends Provider {
 
 	static handleSubmit() {
 		this.getWebview().executeJavaScript(`{
-		var btn = document.querySelector("button[aria-label*='Send Message']"); // subsequent screens use this
-    if (!btn) var btn = document.querySelector('button:has(div svg)'); // new chats use this
-    if (!btn) var btn = document.querySelector('button:has(svg)'); // last ditch attempt
+		var btn = document.querySelector('button[aria-label="Send message"]')
 		if (btn) {
-			btn.focus();
 			btn.disabled = false;
 			btn.click();
 		}
@@ -31,28 +27,7 @@ class Claude extends Provider {
 	}
 
 	static handleCss() {
-		this.getWebview().addEventListener('dom-ready', () => {
-			// hide message below text input, sidebar, suggestions on new chat
-			setTimeout(() => {
-				this.getWebview().insertCSS(`
-        /* hide the claude avatar in response */
-        .p-1.w-9.h-9.shrink-0 {
-          display: none;
-        }
-        /* reduce claude prompt margins */
-        .mx-4.md\:mx-12.mb-2.md\:mb-4.mt-2.w-auto {
-          margin: 0 !important;
-        }
-
-        `);
-			}, 1000);
-			setTimeout(() => {
-				this.getWebview().executeJavaScript(`{
-				// hide welcome back title
-				document.querySelector('h2').style.display = 'none';
-				}`);
-			}, 1000);
-		});
+		// this.getWebview().addEventListener('dom-ready', () => {});
 	}
 
 	static handleDarkMode(isDarkMode) {
